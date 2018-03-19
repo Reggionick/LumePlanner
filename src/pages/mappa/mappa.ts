086541bc0d9e16b8a4e933998a4104cf8d0a9da8
Nicola Reggiani <nicola.reggiani@reiteco.com>
2018-03-19 18:50:28 +0100

    Removed page's modules

--- src/pages/mappa/mappa.ts
+++ src/pages/mappa/mappa.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import {
   GoogleMaps,
   GoogleMap,
@@ -9,7 +9,6 @@ import {
   MyLocation
 } from '@ionic-native/google-maps';
 
-@IonicPage()
 @Component({
   selector: 'page-mappa',
   templateUrl: 'mappa.html',
