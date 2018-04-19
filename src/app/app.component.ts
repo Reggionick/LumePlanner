796550adb4c5c68638d2ea3d835bdc7fbec347c7
Nicola Reggiani <nicola.reggiani@reiteco.com>
2018-04-19 23:32:20 +0200

    Updated side menu to new specifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,12 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { TranslateService } from "@ngx-translate/core";
 
 import { HomePage } from '../pages/home/home';
-import { MappaPage } from '../pages/mappa/mappa';
 import { MyTripsPage } from '../pages/my-trips/my-trips';
-import { TracksPage } from '../pages/tracks/tracks';
 import { UserPage } from '../pages/user/user';
 import { HelpPage } from '../pages/help/help';
-import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   templateUrl: 'app.html'
@@ -32,12 +30,8 @@ export class MyApp {
 
     translate.use('it');
 
-    // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', translateKey: 'homepage', component: HomePage },
-      { title: 'Mappa', translateKey: 'my_places', component: MappaPage },
       { title: 'MyTrips', translateKey: 'my_itineraries', component: MyTripsPage },
-      { title: 'Tracks', translateKey: 'itineraries', component: TracksPage },
       { title: 'User', translateKey: 'user', component: UserPage },
       { title: 'resting', translateKey: 'help', component: HelpPage }
     ];
@@ -61,6 +55,6 @@ export class MyApp {
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+    this.nav.push(page.component).catch(err => console.log(err));
   }
 }
