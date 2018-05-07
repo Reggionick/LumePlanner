302b5a32cb737142a6b68663d3ecf33aa61842b5
Nicola Reggiani <nicola.reggiani@reiteco.com>
2018-05-08 00:38:20 +0200

    Added missing LumeHttp functions

--- src/providers/lume-http/lume-http.ts
+++ src/providers/lume-http/lume-http.ts
@@ -5,6 +5,7 @@ import { Injectable } from '@angular/core';
 export class LumeHttpProvider {
 
   ditaServer = 'http://lume.morselli.unimore.it/DITA/WS/';
+  ditaServerFiles = 'http://lume.morselli.unimore.it/DITA/WS/files/';
 
   user: string;
 
@@ -33,6 +34,16 @@ export class LumeHttpProvider {
     return this.http.get(this.ditaServer + "checkuser?userid=" + this.user);
   }
 
+  getLocalize(coord: {lat: number, lon: number}) {
+    return this.http.get(this.ditaServer + "localize?user=" + this.user + "&lat=" + coord.lat + "&lon=" + coord.lon);
+  }
+
+  getRoute(fromCoord: {lat: number, lon: number}, toCoord: {lat: number, lon: number}) {
+    const start = fromCoord.lat + "," + fromCoord.lon;
+    const end = toCoord.lat + "," + toCoord.lon;
+    return this.http.get(this.ditaServer + "http://lume.morselli.unimore.it/DITA/WS/route?vehicle=foot&start=" + start + "&end=" + end);
+  }
+
   postPreferences(preferences: any) {
     const jsonObj = {
       user: this.user,
@@ -49,5 +60,25 @@ export class LumeHttpProvider {
   postAcceptPlan(plan: any) {
     plan.user = this.user;
     return this.http.post(this.ditaServer + "accept_plan", plan);
+  }
+
+  postVisit(city: string, activity: any) {
+    const jsonObj = {
+      user: this.user,
+      visited: activity,
+      city: city,
+      time: new Date().getTime(),
+      rating: 10
+    };
+    return this.http.post(this.ditaServer + "visited", jsonObj);
+  }
+
+  postFinish() {
+    return this.http.post(this.ditaServer + "finish", this.user);
+  }
+
+
+  getCrowd(city: string) {
+    return this.http.get(this.ditaServerFiles + "data/" + city + "/crowd.json");
   }
 }
