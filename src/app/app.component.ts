08e7a71614d0f62c5c208ce043e4ab7da13d0c1b
Nicola Reggiani <nicola.reggiani@reiteco.com>
2018-05-11 12:52:09 +0200

    Added language chooser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -60,4 +60,9 @@ export class MyApp {
     // we wouldn't want the back button to show in this scenario
     this.nav.push(page.component).catch(err => console.log(err));
   }
+
+  cambioLingua(newLang) {
+    this.translate.use(newLang);
+
+  }
 }
