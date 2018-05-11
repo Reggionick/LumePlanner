3d40698af14477e1b146ab38428cad407c4ae87b
Nicola Reggiani <nicola.reggiani@reiteco.com>
2018-05-11 12:41:23 +0200

    Completed Activity page

--- src/pages/activity/activity.ts
+++ src/pages/activity/activity.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {GoogleMap, GoogleMaps, GoogleMapsEvent} from "@ionic-native/google-maps";
 
 @Component({
   selector: 'page-activity',
   templateUrl: 'activity.html',
 })
 export class ActivityPage {
+
+  map: GoogleMap;
 
   activity: any;
   activityName: string;
@@ -19,7 +22,33 @@ export class ActivityPage {
   }
 
   ionViewDidLoad() {
+    this.loadMap();
+  }
 
+  loadMap() {
+    this.map = GoogleMaps.create('activity_map', {
+      controls: {
+        myLocation: true,
+        myLocationButton: true
+      },
+      camera: {
+        target: {
+          lng: this.activity.geometry.coordinates[0],
+          lat: this.activity.geometry.coordinates[1]
+        },
+        zoom: 17
+      }
+    });
+
+    this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
+      this.map.addMarker({
+        position: {
+          lng: this.activity.geometry.coordinates[0],
+          lat: this.activity.geometry.coordinates[1]
+        },
+        title: this.activity.display_name
+      });
+    }) ;
   }
 
 }
